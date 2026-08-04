d6de31205ace21748843a02ffe664d806ff8e230
Kazuki Matsuda <mats.kazuki@gmail.com>
2026-08-04 13:21:23 +0900

    Improve menu bar meeting visibility (#243)

--- Sources/Dahlia/Views/MenuBarCalendarParticipationIndicator.swift
+++ Sources/Dahlia/Views/MenuBarCalendarParticipationIndicator.swift
@@ -5,28 +5,23 @@ struct MenuBarCalendarParticipationIndicator: View {
     let isAttending: Bool
 
     var body: some View {
-        Image(nsImage: isAttending ? Self.solidIndicator : Self.dottedIndicator)
+        Image(nsImage: isAttending ? Self.attendingIndicator : Self.notAttendingIndicator)
             .renderingMode(.template)
             .accessibilityHidden(true)
     }
 
-    private static let solidIndicator = makeIndicator(isSolid: true)
-    private static let dottedIndicator = makeIndicator(isSolid: false)
+    private static let attendingIndicator = makeIndicator(isAttending: true)
+    private static let notAttendingIndicator = makeIndicator(isAttending: false)
 
-    private static func makeIndicator(isSolid: Bool) -> NSImage {
+    private static func makeIndicator(isAttending: Bool) -> NSImage {
         let image = NSImage(size: NSSize(width: 6, height: 14), flipped: true) { _ in
-            NSColor.black.setFill()
-            if isSolid {
-                NSBezierPath(
-                    roundedRect: NSRect(x: 2, y: 1, width: 2, height: 12),
-                    xRadius: 1,
-                    yRadius: 1
-                ).fill()
-            } else {
-                for y in [1.5, 6, 10.5] {
-                    NSBezierPath(ovalIn: NSRect(x: 2, y: y, width: 2, height: 2)).fill()
-                }
-            }
+            let width: CGFloat = isAttending ? 4 : 2
+            NSColor.black.withAlphaComponent(isAttending ? 1 : 0.35).setFill()
+            NSBezierPath(
+                roundedRect: NSRect(x: (6 - width) / 2, y: 1, width: width, height: 12),
+                xRadius: width / 2,
+                yRadius: width / 2
+            ).fill()
             return true
         }
         image.isTemplate = true
