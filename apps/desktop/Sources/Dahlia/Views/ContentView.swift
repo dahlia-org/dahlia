8c28d1d62f6c834ccea422bea834e133d66eaa80
mats <mats.kazuki@gmail.com>
2026-08-31 14:39:08 +0900

    フルページチャット遷移のハングを修正

--- apps/desktop/Sources/Dahlia/Views/ContentView.swift
+++ apps/desktop/Sources/Dahlia/Views/ContentView.swift
@@ -38,6 +38,7 @@ struct ContentView: View {
             isVisible: chatCoordinator.isDockedVisible
                 && !isShowingFullScreenChat
                 && !isShowingSettings,
+            animatesVisibilityChanges: !isShowingFullScreenChat,
             onWidthChange: mainWindowNavigation.updateChatSidebarWidth
         ) {
             Group {
